ccf3b4c4036daa241286af19b53b9bf68f253713
Thomas Cameron <thomas.cameron@camerontech.com>
2025-04-16 03:32:43 -0500

    tf updates

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,11 +1,10 @@
-vpc_cidr             = "10.0.0.0/16"
-public_subnet_cidr   = "10.0.1.0/24"
-private_subnet_cidr  = "10.0.2.0/24"
+stig_ami_id      = "ami-0dee8d0e6a88728d8"
+standard_ami_id  = "ami-0672bca11b972c7d0"
+instance_type    = "t3.medium"
+key_name         = "demo-key"
 
-stig_ami_id          = "ami-0dee8d0e6a88728d8"
-standard_ami_id      = "ami-0672bca11b972c7d0"
-instance_type        = "t3.medium"
-
-# Only applies to STIG EC2s since IdM/Keycloak are individual resources
-instance_count       = 2
+vpc_cidr              = "10.31.0.0/16"
+public_subnet_cidr    = "10.31.1.0/24"
+private_subnet_cidr   = "10.31.2.0/24"
+aws_region            = "us-east-2"
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,35 +1,45 @@
 variable "aws_region" {
   description = "AWS region"
+  type        = string
   default     = "us-east-2"
 }
 
 variable "vpc_cidr" {
-  default = "10.31.0.0/16"
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.31.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-  default = "10.31.1.0/24"
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.31.1.0/24"
 }
 
 variable "private_subnet_cidr" {
-  default = "10.31.2.0/24"
-}
-
-variable "instance_count" {
-  default = 4
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.31.2.0/24"
 }
 
 variable "instance_type" {
-  default = "t3.medium"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.medium"
 }
 
 variable "stig_ami_id" {
-  type        = string
   description = "AMI ID for STIG-hardened instances"
+  type        = string
 }
 
 variable "standard_ami_id" {
-  type        = string
   description = "AMI ID for standard RHEL instances (IdM, Keycloak)"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key name to use for EC2 instances"
+  type        = string
 }
 
