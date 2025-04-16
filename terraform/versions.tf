bcd18834019ce5e87c91eaa6a896b8c33bbf81e3
Thomas Cameron <thomas.cameron@camerontech.com>
2025-04-15 20:08:48 -0500

    updated several files

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,13 +1,11 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.4.6"
 
-  backend "s3" {
-    bucket = "rhel-compliance-tfstate"
-    key    = "infrastructure/terraform.tfstate"
-    region = "us-east-2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
-provider "aws" {
-  region = var.aws_region
-}
